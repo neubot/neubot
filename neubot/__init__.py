29e420a0808aec2ccdc486227327705d3cdb0c36
Simone Basso <bassosimone@gmail.com>
2010-06-06 17:16:47 +0200

    Remove old and unused neubot/clients.py neubot/servers.py

--- neubot/__init__.py
+++ neubot/__init__.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
 
-from clients import *
-from servers import *
 from utils import *
 
 import auto
