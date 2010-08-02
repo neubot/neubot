2e0d9bf4b5a11f1a6d65ac0ae644ac53e9430dd5
Simone Basso <bassosimone@gmail.com>
2010-08-02 10:43:08 +0200

    neubot.statusicon: Manage status icon in the notification area
    The notification area icon invokes gtk.loop() and so we cannot
    invoke neubot.net.loop() or we will stop processing Gtk events as
    long as we have I/O.  So, we decided to work around this problem
    by configuring PyGtk to invoke dispatch() periodically--not the
    best solution on earth, but it is simple and it "just works."
    
    There is more work to do--this patch is suitable for Ubuntu and
    for other systems where PyGtk is installed _and_ employs Python
    2.6.  So, we need to address the case where PyGtk (or even Gtk)
    is not installed, and we need to decide what to do for Windows
    and Mac as well (is it better to insert PyGtk into the executa-
    ble/App or to leverage some Python interface to the underlying
    platform's API?)

---
+++ neubot/statusicon.py
@@ -0,0 +1,96 @@
+# neubot/statusicon.py
+# Copyright (c) 2010 NEXA Center for Internet & Society
+
+# This file is part of Neubot.
+#
+# Neubot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Neubot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
+
+# Portions of this file are a derivative work of portions of:
+#Facebook Notify - Facebook status notifier for GNOME
+#Copyright (C) 2009 John Stowers <john.stowers@gmail.com>
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# Status icon for notification area
+#
+
+import sys
+
+if __name__ == "__main__":
+    sys.path.append(".")
+
+import gtk
+import gobject
+
+import neubot
+
+TIMEOUT = 250
+URI = "http://127.0.0.1:9774/state"
+
+class TrayIcon:
+    def __init__(self):
+        self.needsend = True
+        self.tray = gtk.StatusIcon()
+        self.tray.set_from_icon_name(gtk.STOCK_NETWORK)                 # XXX
+        self.tray.connect("popup-menu", self.on_popup_menu)
+        self.tray.connect("activate", self.on_activate)
+        self.tray.set_visible(False)
+        self.update()
+
+    def on_popup_menu(self, status, button, time):
+        pass
+
+    def on_activate(self, *args):
+        pass
+
+    def update(self):
+        if self.needsend:
+            m = neubot.http.compose(method="GET", uri=URI, keepalive=False)
+            neubot.http.send(m, sent=self.sent, cantsend=self.cantsend)
+        neubot.net.dispatch()
+        gobject.timeout_add(TIMEOUT, self.update)
+
+    def cantsend(self, m):
+        self.needsend = True
+
+    def sent(self, m):
+        neubot.http.recv(m, received=self.received, cantrecv=self.cantrecv)
+
+    def received(self, m):
+        if m.code == "200":
+            body = m.body.read().strip()                                # XXX
+            if body != "SLEEPING":
+                self.tray.set_visible(True)
+                #self.tray.set_blinking(True)
+        self.needsend = True
+
+    def cantrecv(self, m):
+        self.needsend = True
+
+if __name__ == "__main__":
+    gtk.gdk.threads_init()
+    app = TrayIcon()
+    gtk.main()
