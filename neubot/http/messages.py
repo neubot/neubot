312cace8d4a71330e0efbba13ab1d6e6edae105b
Simone Basso <bassosimone@gmail.com>
2010-08-17 18:02:21 +0200

    neubot.http: messages: Make the code PEP8-compliant

--- neubot/http/messages.py
+++ neubot/http/messages.py
@@ -19,79 +19,82 @@
 import StringIO
 import types
 
-class message:
-	def __init__(self, method="", uri="", code="", reason="", protocol=""):
-		self.method = method
-		self.uri = uri
-		self.code = code
-		self.reason = reason
-		self.protocol = protocol
-		self.headers = {}
-		self.body = StringIO.StringIO("")
-		self._proto = None
-		self.scheme = ""
-		self.address = ""
-		self.port = ""
-		self.pathquery = ""
-		self.family = 0
-		self.context = None
-		if (self.method and self.code):
-			raise (ValueError("Both method and code are set"))
+class Message:
+    def __init__(self, method="", uri="", code="", reason="", protocol=""):
+        self.method = method
+        self.uri = uri
+        self.code = code
+        self.reason = reason
+        self.protocol = protocol
+        self.headers = {}
+        self.body = StringIO.StringIO("")
+        self._proto = None
+        self.scheme = ""
+        self.address = ""
+        self.port = ""
+        self.pathquery = ""
+        self.family = 0
+        self.context = None
+        if self.method and self.code:
+            raise ValueError("Both method and code are set")
 
-	def serialize_headers(self):
-		if (not self.method and not self.code):
-			raise (Exception("Not initialized"))
-		lst = []
-		if (self.method):
-			lst.append(self.method)
-			lst.append(" ")
-			if not self.uri.startswith("/"):
-				if self.pathquery.startswith("/"):
-					lst.append(self.pathquery)
-				else:
-					lst.append("/")
-			else:
-				lst.append(self.uri)
-			lst.append(" ")
-			lst.append(self.protocol)
-		else:
-			lst.append(self.protocol)
-			lst.append(" ")
-			lst.append(self.code)
-			lst.append(" ")
-			lst.append(self.reason)
-		lst.append("\r\n")
-		for key, value in self.headers.items():
-			lst.append(key)
-			lst.append(": ")
-			lst.append(value)
-			lst.append("\r\n")
-		lst.append("\r\n")
-		octets = "".join(lst)
-		return (StringIO.StringIO(octets))
+    def serialize_headers(self):
+        if not self.method and not self.code:
+            raise Exception("Not initialized")
+        lst = []
+        if self.method:
+            lst.append(self.method)
+            lst.append(" ")
+            if not self.uri.startswith("/"):
+                if self.pathquery.startswith("/"):
+                    lst.append(self.pathquery)
+                else:
+                    lst.append("/")
+            else:
+                lst.append(self.uri)
+            lst.append(" ")
+            lst.append(self.protocol)
+        else:
+            lst.append(self.protocol)
+            lst.append(" ")
+            lst.append(self.code)
+            lst.append(" ")
+            lst.append(self.reason)
+        lst.append("\r\n")
+        for key, value in self.headers.items():
+            lst.append(key)
+            lst.append(": ")
+            lst.append(value)
+            lst.append("\r\n")
+        lst.append("\r\n")
+        octets = "".join(lst)
+        return StringIO.StringIO(octets)
 
-	def serialize_body(self):
-		return (self.body)
+    def serialize_body(self):
+        return self.body
 
-	def __getitem__(self, key):
-		if (type(key) != types.StringType):
-			raise (TypeError("key should be a string"))
-		key = key.lower()
-		if (self.headers.has_key(key)):
-			return (self.headers[key])
-		return ("")
+    def __getitem__(self, key):
+        if type(key) != types.StringType:
+            raise TypeError("key should be a string")
+        key = key.lower()
+        if self.headers.has_key(key):
+            return self.headers[key]
+        return ""
 
-	def __setitem__(self, key, value):
-		if (type(key) != types.StringType):
-			raise (TypeError("key should be a string"))
-		key = key.lower()
-		if (self.headers.has_key(key)):
-			value = self.headers[key] + ", " + value
-		self.headers[key] = value
+    def __setitem__(self, key, value):
+        if type(key) != types.StringType:
+            raise TypeError("key should be a string")
+        key = key.lower()
+        if self.headers.has_key(key):
+            value = self.headers[key] + ", " + value
+        self.headers[key] = value
 
-	def __delitem__(self, key):
-		if (type(key) != types.StringType):
-			raise (TypeError("key should be a string"))
-		key = key.lower()
-		if (self.headers.has_key(key)):
-			del self.headers[key]
+    def __delitem__(self, key):
+        if type(key) != types.StringType:
+            raise TypeError("key should be a string")
+        key = key.lower()
+        if self.headers.has_key(key):
+            del self.headers[key]
+
+# For compatibility with existing code
+message = Message
