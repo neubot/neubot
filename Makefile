af7971e8bc6fd8df1ffb5e26d7c2cd5e43811389
Simone Basso <bassosimone@gmail.com>
2010-09-02 17:27:38 +0200

    Release neubot/0.2.3

--- Makefile
+++ Makefile
@@ -20,7 +20,7 @@
 # Makefile
 #
 
-VERSION	= 0.2.2
+VERSION	= 0.2.3
 
 DEB     = dist/neubot-$(VERSION)-1_all.deb
 DESTDIR =
--- neubot/__init__.py
+++ neubot/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
 
-version = "0.2.2"
+version = "0.2.3"
 
 import config
 
