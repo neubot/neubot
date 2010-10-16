bf157cde267203371766125514291d51efdc2bd5
Simone Basso <bassosimone@gmail.com>
2010-10-16 19:45:00 +0200

    Build.but: document how win32 build system works

--- Build.bat
+++ Build.bat
@@ -1,5 +1,12 @@
+rem create dist/neubot.exe and stuff
 python setup.py py2exe --bundle-files 2
+
+rem copy web user interface files into dist/
 mkdir dist\www
 copy neubot\www dist\www
+
+rem create Neubot_Setup_x.y.z.exe
 "c:\program files\nsis\makensis.exe" neubot.nsi
+
+rem do not hide possible errors
 pause
