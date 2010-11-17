78fa9f762c6e01a6b74012aab67a6d419846e006
Simone Basso <bassosimone@gmail.com>
2010-11-17 01:12:01 +0100

    [bittorent] bittorrent: initial experimental support for BitTorrent
    At this stage of development we can create a BitTorrent server and
    connect it with a BitTorrent client, handshake, and the the connection
    is closed by both parties.  Huge portions of the code come from the
    last opensource public release of BitTorrent mainline.
    
    More in detail, this commit adds the following files:
    
      bittorrent/bitfield.py
    
        Nearly unmodified BTL/bitfield.py, released under Python
        License v2.5;
    
      bittorrent/connector.py
    
        Based on BitTorrent/Connector.py (GPLv3).  Most of the code
        has been commented out and now the module just provides bare
        BitTorrent support.  We will re-enable features in future
        releases;
    
      bittorrent/handler.py
    
        Definition of Handler and re-implementation of ConnectionWrapper
        from BitTorrent/RawServer_twisted.py (GPLv3).  ConnectionWrapper has
        been renamed StreamWrapper and implements the connection API using
        neubot.net API;
    
      bittorrent/wrapper.py
    
        This is the high level code that controls the bittorrent module
        and provides a main() for testing.

---
+++ neubot/bittorrent/handler.py
@@ -0,0 +1,83 @@
+# neubot/bittorrent/handler.py
+
+# Originally written by Greg Hazel
+# Modified by Simone Basso
+
+# This file is part of Neubot.
+#
+# Neubot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Neubot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
+
+# Kudos to BitTorrent developers: this is a nice interface!
+class Handler:
+    def connection_starting(self, addr):
+        pass
+
+    def connection_made(self, s):
+        pass
+
+    def connection_failed(self, s, exception):
+        pass
+
+    def data_came_in(self, addr, data):
+        pass
+
+    def connection_flushed(self, s):
+        pass
+
+    def connection_lost(self, s):
+        pass
+
+# BitTorrent interface but neubot engine!
+class StreamWrapper:
+    def __init__(self, stream):
+        self.handler = None
+        self.stream = stream
+        self.stream.notify_closing = self._closing
+        self.ip, self.port = stream.peername[:2]
+        # IIRC Twisted use an integer to save the port
+        self.port = int(self.port)
+        self.closed = False
+
+    def write(self, data):
+        if self.closed:
+            return
+        # bleh
+        if isinstance(data, buffer):
+            data = str(data)
+        self.stream.send(data, self._written)
+
+    def _written(self, stream, data):
+        self.handler.connection_flushed(self)
+
+    def close(self):
+        if self.closed:
+            return
+        self.stream.close()
+
+    def _closing(self):
+        self.closed = True
+        self.handler = None
+        self.stream.notify_closing = None
+        self.ip, self.port = None, None
+        self.stream = None
+
+    def attach_connector(self, handler):
+        self.handler = handler
+        self.stream.recv(8000, self._data_came_in)
+
+    def _data_came_in(self, stream, data):
+        self.handler.data_came_in((self.ip, self.port), data)
+        if self.closed:
+            return
+        self.stream.recv(8000, self._data_came_in)
