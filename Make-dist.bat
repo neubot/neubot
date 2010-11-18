7507eac689c36daae69d4c6c40e359864bb1ce7d
Simone Basso <bassosimone@gmail.com>
2010-11-18 15:27:25 +0100

    [testing] Make-dist.bat: make sure we install images and jqplot

--- Make-dist.bat
+++ Make-dist.bat
@@ -3,5 +3,9 @@ copy win32\neubot-headless.exe dist\
 copy win32\neubot-start.exe dist\
 copy win32\neubot-stop.exe dist\
 mkdir dist\www
+mkdir dist\www\images
+mkdir dist\www\jqplot
 copy neubot\www dist\www
+copy neubot\www\images dist\www\images
+copy neubot\www\jqplot dist\www\jqplot
 pause
