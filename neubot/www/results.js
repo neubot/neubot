e956de47c5c12be9b71f6067a9a3adc9fe994a14
Simone Basso <bassosimone@gmail.com>
2010-11-18 11:58:06 +0100

    [www-jqplot] results: use jqplot and plot recent results
    While at it, also make sure that neubot logo links to the website
    and add a link to return to the home page (similarly to what the
    home page already does).

--- neubot/www/results.js
+++ neubot/www/results.js
@@ -4,6 +4,16 @@
  * License: GNU GPLv3
  */
 
+/* use a function for the exact format desired... */
+function LocaleDateString(d){
+    function pad(n) {
+        return n < 10 ? '0' + n : n
+    }
+    return d.getFullYear() + '-' + pad(d.getMonth() + 1) + '-'
+      + pad(d.getDate()) + '\n' + pad(d.getHours()) + ':'
+      + pad(d.getMinutes());
+}
+
 function getresults(data){
     var html = "";
     var fields = ["timestamp", "internalAddress", "realAddress",
@@ -12,7 +22,14 @@ function getresults(data){
     var titles = ["Timestamp", "Internal Address", "Real Address",
                   "Remote Address", "Connect Time", "Latency",
                   "Download Speed", "Upload Speed"];
-    var i, n = document.getElementById("step").range[0];
+    var i, nn, n = document.getElementById("step").range[0];
+
+    var download = [], upload = [];
+    var ipCounter = {}, ipCounterN = 0;
+    var downloadLabels = [ ], uploadLabels = [];
+    var plotTicks = [];
+
+    nn = n;
 
     if (n == 0) {
         $('#prev').addClass("disabled");
@@ -54,13 +71,34 @@ function getresults(data){
         html += '<tr class="'+ trclass + '">';
 
         /*
-         * Fill the row.  XXX We should scale the unit depending
-         * on the actual speed, latency.
+         * Fill the row and gather data for the plot.
+         * XXX We should scale the unit depending on the actual
+         * speed, latency.
          */
 
         for (i = 0; i < fields.length; i++) {
             val = $entry.find(fields[i]).text();
             switch(fields[i]) {
+                case 'realAddress':
+                    var address = val;
+                    if (ipCounter[address] == undefined) {
+                        ipCounter[address] = ipCounterN;
+                        download[ipCounterN] = [];
+                        upload[ipCounterN] = [];
+                        downloadLabels[ipCounterN] = {
+                          label: "UP " + address,
+                          markerOptions: {style:'square'},
+                          neighborThreshold: -1
+                        };
+                        uploadLabels[ipCounterN] = {
+                          label: "DOWN " + address,
+                          markerOptions:{style:'circle'},
+                          neighborThreshold: -1
+                        };
+                        ipCounterN++;
+                    }
+                    txt = address;
+                    break;
                 case 'timestamp':
                     var timestamp = new Date(val*1000);
                     var rg = new RegExp("GMT");
@@ -70,6 +108,10 @@ function getresults(data){
                      * XXX IIUIC here we use GMT which could
                      * be a bit misleading for users.
                      */
+                    plotTicks.unshift([
+                      Number(n-nn),
+                      LocaleDateString(timestamp)
+                    ]);
                     txt = n++ + " " + timestamp.toString().replace(/GMT.*$/,"");
                     break;
                 case 'connectTime':
@@ -77,14 +119,27 @@ function getresults(data){
                     txt = (val * 1000).toFixed(0) + " ms";
                     break;
                 case 'downloadSpeed':
+                    txt = (val * 8/1024/1024).toFixed(3);
+                    download[ipCounter[address]].unshift([
+                      Number(n-nn),
+                      Number(txt)
+                    ]);
+                    txt += " Mb/s";
+                    break;
                 case 'uploadSpeed':
-                    txt = (val * 8/1024/1024).toFixed(3) + " Mb/s";
+                    txt = (val * 8/1024/1024).toFixed(3);
+                    upload[ipCounter[address]].unshift([
+                      Number(n-nn),
+                      Number(txt)
+                    ]);
+                    txt += " Mb/s";
                     break;
                 default:
                     txt = val;
                     break;
             }
-            html += '<td>' + txt + '</td>';
+            if (txt != "")
+                html += '<td>' + txt + '</td>';
         }
 
         html += "</tr>"
@@ -100,11 +155,55 @@ function getresults(data){
     } else {
         $('#next').removeClass("disabled");
     }
+
+    /*
+     * Create and show the plot
+     */
+
+    var bandwidthPlot = $.jqplot(
+      "chartdiv1",
+      download.concat(upload), {
+        axesDefaults: {
+          tickRenderer: $.jqplot.CanvasAxisTickRenderer,
+        },
+        title: {
+          text: "Download and Upload Rate"
+        },
+        axes: {
+          xaxis: {
+            label: "Time",
+            ticks: plotTicks,
+            tickOptions: {
+              angle: -90,
+              fontSize: '10pt', showMark: true
+            },
+            showTickMarks: true
+          },
+          yaxis: {
+            label: "Mb/s",
+            min: 0
+          }
+        },
+        legend: {
+          show: true
+        },
+        cursor: {
+          showVerticalLine:false,
+          showHorizontalLine:true,
+          showCursorLegend: false,
+          showTooltip: false,
+          tooltipLocation: 'sw',
+          zoom: true
+        },
+        highlighter: {show: false},
+        series: downloadLabels.concat(uploadLabels)
+      });
+
+    bandwidthPlot.replot();
 }
 
 $(document).ready(function() {
-    /* XXX */
-    $("#content").prepend('<center><h2>Speedtest results - <a id="prev" href="#">&lt;&lt;</a>&nbsp;<input id="step" type="text" value="20" size="1" style="text-align: center"/>&nbsp;<a id="next" href="#">&gt;&gt;</a></h2></center><br>');
+    $.jqplot.config.enablePlugins = true;
 
     var elm = document.getElementById("step");
     var inc = Number($('#step').val());
@@ -112,6 +211,24 @@ $(document).ready(function() {
 
     elm.range = [ start, stop ];
     $.get("/api/results?start=" + start + "&stop=" + stop, getresults);
+
+    /*
+     * TODO The API to access results it way too low level, and it
+     * would be much better to access time ranges, e.g. gimme all the
+     * results in the last month.
+     * TODO Another quirk is that the API just returns the cache but
+     * we want to access the whole data set.
+     */
+
+    $("#step").keyup(function(event){
+      if(event.keyCode == 13) {
+        inc = Number(elm.value);
+        stop = start + inc;
+        elm.range = [ start, stop ];
+        $.get("/api/results?start=" + start + "&stop=" + stop, getresults);
+        return false;
+      }
+    });
 
     $('#prev').click(function() {
         inc = Number(elm.value);
