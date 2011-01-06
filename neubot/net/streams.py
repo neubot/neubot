6d7e1a28e74d6a089955584222e2d14cbc9c5974
Simone Basso <bassosimone@gmail.com>
2011-01-07 00:41:51 +0100

    net/streams.py: import sys instead of from sys import ...

--- neubot/net/streams.py
+++ neubot/net/streams.py
@@ -24,9 +24,10 @@
 # Asynchronous I/O for non-blocking sockets (and SSL)
 #
 
+import sys
+
 if __name__ == "__main__":
-    from sys import path
-    path.insert(0, ".")
+    sys.path.insert(0, ".")
 
 from neubot.net.pollers import Pollable
 from types import UnicodeType
@@ -461,9 +462,7 @@ from socket import AF_INET6
 from socket import AF_INET
 from socket import socket
 from neubot import log
-from sys import exit
 from os import strerror
-from sys import argv
 
 #
 # We have the same code path for connect_ex() returning 0 and returning
@@ -578,7 +577,6 @@ from socket import SO_REUSEADDR
 from socket import SOL_SOCKET
 from socket import socket
 from socket import AF_INET
-from sys import exit
 from neubot import log
 
 LISTENARGS = {
@@ -698,31 +696,26 @@ class Source:
     def __del__(self):
         pass
 
-from sys import argv
-from sys import stdout
-from sys import stderr
-from sys import exit
-
 from neubot.net.listeners import listen
 from neubot.net.connectors import connect
 from neubot.net.pollers import loop
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        stdout.write("Usage: %s discard|echo|source\n" % argv[0])
-        exit(1)
-    elif argv[1] == "discard":
+    if len(sys.argv) != 2:
+        sys.stdout.write("Usage: %s discard|echo|source\n" % sys.argv[0])
+        sys.exit(1)
+    elif sys.argv[1] == "discard":
         listen("127.0.0.1", "8009", accepted=Discard)
         loop()
-        exit(0)
-    elif argv[1] == "echo":
+        sys.exit(0)
+    elif sys.argv[1] == "echo":
         listen("127.0.0.1", "8007", accepted=Echo)
         loop()
-        exit(0)
-    elif argv[1] == "source":
+        sys.exit(0)
+    elif sys.argv[1] == "source":
         connect("127.0.0.1", "8009", connected=Source)
         loop()
-        exit(0)
+        sys.exit(0)
     else:
-        stderr.write("Usage: %s discard|echo|source\n" % argv[0])
-        exit(1)
+        sys.stderr.write("Usage: %s discard|echo|source\n" % sys.argv[0])
+        sys.exit(1)
