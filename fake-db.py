64949ca428982033a4a293df89f7653075003ebe
Roberto D'Auria <everlastingfire@autistici.org>
2011-01-08 17:32:51 +0100

    fake-db.py: improved fake rows generation

--- fake-db.py
+++ fake-db.py
@@ -24,21 +24,49 @@
 # Create fake database for testing
 #
 
+import sys
+import uuid
+import time
+import random
 from subprocess import call
 from sqlite3 import connect
-from sys import stderr
-from sys import argv
+
+ROWS = 10
+
+RESULTS_SAVE = "INSERT INTO results VALUES(null, :tag, :result, :timestamp, :ident);"
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        stderr.write("Usage: %s database\n" % argv[0])
-        exit(1)
-    call(["/usr/bin/env", "python", "neubot/database.py", "-i", argv[1]])
-    connection = connect(argv[1])
+    if len(sys.argv) != 2:
+        stderr.write("Usage: %s database\n" % sys.argv[0])
+        sys.exit(1)
+    call(["/usr/bin/env", "python", "neubot/database.py", "-i", sys.argv[1]])
+    connection = connect(sys.argv[1])
     cursor = connection.cursor()
     count = 0
-    while count < 16384:
-        cursor.execute('insert into results values(null,"x","x",1,"x");')
+    tag = "speedtest"
+    ident = str(uuid.uuid4())
+    timestamp = int(time.time())
+    remote = "130.192.91.211"
+
+    while count < ROWS:
+        addr = "10.0.0." + str(random.randint(1, 254))
+        download = str(random.random() * 100000)
+        upload = str(random.random() * 40000)
+        result = """\
+<SpeedtestCollect>\
+<client>%s</client>\
+<timestamp>%s</timestamp>\
+<internalAddress>%s</internalAddress>\
+<realAddress>%s</realAddress>\
+<remoteAddress>%s</remoteAddress>\
+<connectTime>%s</connectTime>\
+<latency>%s</latency>\
+<downloadSpeed>%s</downloadSpeed>\
+<uploadSpeed>%s</uploadSpeed>\
+</SpeedtestCollect>""" % (ident, timestamp, addr, addr, remote, str(random.random()),
+            str(random.random()), download, upload)
+        cursor.execute(RESULTS_SAVE, {'tag': tag, 'result': result,
+            'timestamp': timestamp, 'ident': ident})
         count = count + 1
     connection.commit()
     cursor.close()
