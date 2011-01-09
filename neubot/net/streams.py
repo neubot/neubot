8902060b05b80d2effe5ed8fbc009eee41612513
Simone Basso <bassosimone@gmail.com>
2011-01-09 19:02:09 +0100

    Simplify net module: Copy code of test.py into streams.py.
    Pass two of a nearly zero effort strategy to get rid of the ugly
    file neubot/net/test.py.
    
    This file is here because the code it cointains cannot live into
    neubot/net/streams.py.  Indeed the code in test.py depends on the
    code in streams.py as well as from the code in connectors.py and
    listeners.py.  But, in turn, the code in connectors.py and in
    listeners.py depends on streams.py.  So, here there is a circular
    dependency.
    
    To unbreak that, first pass is to move the relevant code from
    connectors.py and listeners.py into streams.py.  And the second
    pass is to move code from test.py into streams.py.  Finally,
    we get rid of test.py.

--- neubot/net/streams.py
+++ neubot/net/streams.py
@@ -24,6 +24,10 @@
 # Asynchronous I/O for non-blocking sockets (and SSL)
 #
 
+if __name__ == "__main__":
+    from sys import path
+    path.insert(0, ".")
+
 from neubot.net.pollers import Pollable
 from types import UnicodeType
 from neubot.utils import ticks
@@ -660,3 +664,70 @@ class Listener(Pollable):
 
 def listen(address, port, accepted, **kwargs):
     Listener(address, port, accepted, **kwargs)
+
+#
+# Unit test
+#
+
+class Discard:
+    def __init__(self, stream):
+        stream.recv(8000, self.got_data)
+
+    def got_data(self, stream, octets):
+        stream.recv(8000, self.got_data)
+
+    def __del__(self):
+        pass
+
+class Echo:
+    def __init__(self, stream):
+        stream.recv(8000, self.got_data)
+
+    def got_data(self, stream, octets):
+        stream.send(octets, self.sent_data)
+
+    def sent_data(self, stream, octets):
+        stream.recv(8000, self.got_data)
+
+    def __del__(self):
+        pass
+
+class Source:
+    def __init__(self, stream):
+        self.buffer = "A" * 8000
+        stream.send(self.buffer, self.sent_data)
+
+    def sent_data(self, stream, octets):
+        stream.send(self.buffer, self.sent_data)
+
+    def __del__(self):
+        pass
+
+from sys import argv
+from sys import stdout
+from sys import stderr
+from sys import exit
+
+from neubot.net.listeners import listen
+from neubot.net.connectors import connect
+from neubot.net.pollers import loop
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        stdout.write("Usage: %s discard|echo|source\n" % argv[0])
+        exit(1)
+    elif argv[1] == "discard":
+        listen("127.0.0.1", "8009", accepted=Discard)
+        loop()
+        exit(0)
+    elif argv[1] == "echo":
+        listen("127.0.0.1", "8007", accepted=Echo)
+        loop()
+        exit(0)
+    elif argv[1] == "source":
+        connect("127.0.0.1", "8009", connected=Source)
+        loop()
+        exit(0)
+    else:
+        stderr.write("Usage: %s discard|echo|source\n" % argv[0])
+        exit(1)
