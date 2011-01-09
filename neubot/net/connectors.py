b7f39ecfb828bb6560b8155edd5370b0d22ca6da
Simone Basso <bassosimone@gmail.com>
2011-01-09 20:30:49 +0100

    topic.adjust-pollable-api: Revert all the changes in this branch

--- neubot/net/connectors.py
+++ neubot/net/connectors.py
@@ -28,6 +28,14 @@ if __name__ == "__main__":
     from sys import path
     path.insert(0, ".")
 
+HAVE_SSL = True
+try:
+    from ssl import wrap_socket, SSLError
+except ImportError:
+    HAVE_SSL = False
+    class SSLError(Exception):
+        pass
+
 from neubot.net.streams import create_stream
 from neubot.net.pollers import Pollable, poller
 from neubot.utils import fixkwargs
@@ -46,8 +54,123 @@ from sys import exit
 from os import strerror
 from sys import argv
 
+#
+# We have the same code path for connect_ex() returning 0 and returning
+# one of [EINPROGRESS, EWOULDBLOCK].  This is not very efficient because
+# when it returns 0 we know we are already connected and so it would be
+# more logical not to check for writability.  But there is also value
+# in sharing the same code path, namely that testing is simpler because
+# we don't have to test the [EINPROGRESS, EWOULDBLOCK] corner case.
+#
+
+CONNECTARGS = {
+    "cantconnect" : lambda: None,
+    "connecting"  : lambda: None,
+    "conntimeo"   : 10,
+    "family"      : AF_INET,
+    "poller"      : poller,
+    "secure"      : False,
+}
+
+class Connector(Pollable):
+    def __init__(self, address, port, connected, **kwargs):
+        self.address = address
+        self.port = port
+        self.name = (self.address, self.port)
+        self.connected = connected
+        self.kwargs = fixkwargs(kwargs, CONNECTARGS)
+        self.connecting = self.kwargs["connecting"]
+        self.cantconnect = self.kwargs["cantconnect"]
+        self.poller = self.kwargs["poller"]
+        self.family = self.kwargs["family"]
+        self.secure = self.kwargs["secure"]
+        self.conntimeo = self.kwargs["conntimeo"]
+        self.begin = 0
+        self.sock = None
+        self._connect()
+
+    def __del__(self):
+        pass
+
+    def _connect(self):
+        log.debug("* About to connect to %s:%s" % self.name)
+        try:
+            addrinfo = getaddrinfo(self.address, self.port,
+                                   self.family, SOCK_STREAM)
+            for family, socktype, protocol, cannonname, sockaddr in addrinfo:
+                try:
+                    log.debug("* Trying with %s..." % str(sockaddr))
+                    sock = socket(family, socktype, protocol)
+                    sock.setblocking(False)
+                    error = sock.connect_ex(sockaddr)
+                    # Winsock returns EWOULDBLOCK
+                    if error not in [0, EINPROGRESS, EWOULDBLOCK]:
+                        raise SocketError(error, strerror(error))
+                    self.sock = sock
+                    self.begin = ticks()
+                    self.poller.set_writable(self)
+                    log.debug("* Connection to %s in progress" % str(sockaddr))
+                    self.connecting()
+                    break
+                except SocketError:
+                    log.error("* connect() to %s failed" % str(sockaddr))
+                    log.exception()
+        except SocketError:
+            log.error("* getaddrinfo() %s:%s failed" % self.name)
+            log.exception()
+        if not self.sock:
+            log.error("* Can't connect to %s:%s" % self.name)
+            self.cantconnect()
+
+    def fileno(self):
+        return self.sock.fileno()
+
+    def writable(self):
+        self.poller.unset_writable(self)
+        try:
+            # See http://cr.yp.to/docs/connect.html
+            try:
+                self.sock.getpeername()
+            except SocketError, (code, reason):
+                if code == ENOTCONN:
+                    self.sock.recv(8000)
+                else:
+                    raise
+            if self.secure:
+                if HAVE_SSL:
+                    x = wrap_socket(self.sock, do_handshake_on_connect=False)
+                else:
+                    raise RuntimeError("SSL support not available")
+            else:
+                x = self.sock
+            logname = "with %s:%s" % self.name
+            stream = create_stream(x, self.poller, self.sock.fileno(),
+             self.sock.getsockname(), self.sock.getpeername(), logname)
+            log.debug("* Connected to %s:%s!" % self.name)
+            self.connected(stream)
+        except (SocketError, SSLError):
+            log.error("* Can't connect to %s:%s" % self.name)
+            log.exception()
+            self.cantconnect()
+
+    def writetimeout(self, now):
+        timedout = (now - self.begin >= self.conntimeo)
+        if timedout:
+            log.error("* connect() to %s:%s timed-out" % self.name)
+        return timedout
+
+    def closing(self):
+        log.debug("* closing Connector to %s:%s" % self.name)
+        self.cantconnect()
+
+def connect(address, port, connected, **kwargs):
+    Connector(address, port, connected, **kwargs)
+
+#
+# Test unit
+#
+
 from neubot.net.pollers import loop
-from neubot.net.streams import connect
 from neubot import version
 from getopt import GetoptError
 from getopt import getopt
