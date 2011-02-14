beb977ff6605baa06289696f442aaba458b7a4b6
Simone Basso <bassosimone@gmail.com>
2011-02-14 13:11:47 +0100

    Make-dist.bat: Don't install the content of win32
    We are going to remove the stuff in win32, so adjust the install
    scripts for Windows not to install it.

--- Make-dist.bat
+++ Make-dist.bat
@@ -1,7 +1,4 @@
 "C:\Python27\python.exe" setup.py py2exe --bundle-files 2
-copy win32\neubot-headless.exe dist\
-copy win32\neubot-start.exe dist\
-copy win32\neubot-stop.exe dist\
 mkdir dist\www
 mkdir dist\www\images
 mkdir dist\www\jqplot
