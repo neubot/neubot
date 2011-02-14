327014dcae49a1931765ee7f5741280c890e0b96
Simone Basso <bassosimone@gmail.com>
2011-02-14 15:06:49 +0100

    Make-dist.bat: Make sure we install all of www
    The directory names we need to install have changed and this commit
    updates Make-dist.bat to create and populate directories with the right
    names.
    
    I should have notice this before, sorry.  Anyway, this should unbreak
    web user interface support for windows operating systems.

--- Make-dist.bat
+++ Make-dist.bat
@@ -1,8 +1,10 @@
 "C:\Python27\python.exe" setup.py py2exe --bundle-files 2
 mkdir dist\www
-mkdir dist\www\images
-mkdir dist\www\jqplot
+mkdir dist\www\css
+mkdir dist\www\img
+mkdir dist\www\js
 copy neubot\www dist\www
-copy neubot\www\images dist\www\images
-copy neubot\www\jqplot dist\www\jqplot
+copy neubot\www\css dist\www\css
+copy neubot\www\img dist\www\img
+copy neubot\www\js dist\www\js
 pause
