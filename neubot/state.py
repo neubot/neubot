e45b40476833c26e0e71b51d582278a1abc41434
Simone Basso <bassosimone@gmail.com>
2011-02-15 13:20:26 +0100

    state.py: Rewrite to reduce complexity and use JSON
    This patch rewrites state.py to remove most of the complexity and,
    while on that, to serialize the state using JSON.
    
    State.py was trying to enforce a meaning to the data passed by other
    Neubot modules.  This design violates the end to end principle and
    unsurprisingly the resulting code was complex to read and it was difficult
    to make changes.  The first developer to notice that was Roberto D'Auria,
    when he wrote a patch to pass the `rendezvous failed' event to the
    web user interface.  He prodded me to improve the code in state.py,
    but eventually nothing changed until today, due to other priorities.
    The need to serialize the state using JSON prompted the issue of state.py
    complexity again.  And this time I've had no mercy of the existing code
    and I am rewriting it with this patch.
    
    The new state is a dictionary like this::
    
      {
        "event_name": event_value
      }
    
    Where each event is versioned -- the version being the opaque time of
    the last occurrence of such event.  And where the code returns just the
    events older than a given opaque time, if specified.

--- neubot/state.py
+++ neubot/state.py
@@ -1,7 +1,7 @@
 # neubot/state.py
 
 #
-# Copyright (c) 2010 Simone Basso <bassosimone@gmail.com>,
+# Copyright (c) 2011 Simone Basso <bassosimone@gmail.com>,
 #  NEXA Center for Internet & Society at Politecnico di Torino
 #
 # This file is part of Neubot <http://www.neubot.org/>.
@@ -20,338 +20,109 @@
 # along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-#
-# Track neubot state
-#
-
+import sys
+import time
 import os
 
 if __name__ == "__main__":
-    from sys import path
-    path.insert(0, ".")
+    sys.path.insert(0, ".")
 
+from neubot.notify import T
+from neubot.notify import NOTIFIER
 from neubot.notify import STATECHANGE
-from neubot.notify import get_event_timestamp
-from neubot.utils import versioncmp
-from neubot.times import timestamp
-from neubot.utils import XML_to_stringio
-from xml.dom import minidom
-from neubot.notify import publish
-from neubot import version
+from neubot.compat import json
 
-class StateNegotiate:
-    def __init__(self):
-        self.queuePos = 0
-        self.queueLen = 0
 
-    #
-    # ...
-    #   <queuePos>7</queuePos>
-    #   <queueLen>21</queueLen>
-    # ...
-    #
-
-    def marshal(self, document, parent):
-        element = document.createElement("queuePos")
-        text = document.createTextNode(str(self.queuePos))
-        element.appendChild(text)
-        parent.appendChild(element)
-        element = document.createElement("queueLen")
-        text = document.createTextNode(str(self.queueLen))
-        element.appendChild(text)
-        parent.appendChild(element)
-
-    def set_queueInfo(self, queuePos, queueLen):
-        self.queuePos = queuePos
-        self.queueLen = queueLen
-
-# states
-READY = "ready"
-RUNNING = "running"
-DONE = "done"
-
-class StateTest:
-    def __init__(self, name, tasks=[]):
-        self.timestamp = timestamp()
-        self.current = None
-        self.name = name
-        self.states = {}
-        for task in tasks:
-            self.states[task] = READY
-        self.tasks = tasks
-        self.results = []
-
-    def complete(self):
-        self.states[self.current] = DONE
-        self.current = None
-
-    def set_task(self, task):
-        if self.current:
-            self.states[self.current] = DONE
-        self.states[task] = RUNNING
-        self.current = task
-
-    def append_result(self, tag, value, unit):
-        self.results.append((tag, value, unit))
-
-    #
-    # ...
-    #   <name>speedtest</name>
-    #   <timestamp>1293021960</timestamp>
-    #   <task state="done">latency</task>
-    #   <task state="running">download</task>
-    #   <task state="ready">upload</task>
-    #   <result tag="latency" unit="s">0.0012</result>
-    # ...
-    #
-
-    def marshal(self, document, parent):
-        element = document.createElement("timestamp")
-        text = document.createTextNode(str(self.timestamp))
-        element.appendChild(text)
-        parent.appendChild(element)
-        if self.name:
-            element = document.createElement("name")
-            text = document.createTextNode(self.name)
-            element.appendChild(text)
-            parent.appendChild(element)
-        for task in self.tasks:
-            statex = self.states[task]
-            element = document.createElement("task")
-            element.setAttribute("state", statex)
-            text = document.createTextNode(task)
-            element.appendChild(text)
-            parent.appendChild(element)
-        for tag, value, unit in self.results:
-            element = document.createElement("result")
-            element.setAttribute("tag", tag)
-            element.setAttribute("unit", unit)
-            text = document.createTextNode(str(value))
-            element.appendChild(text)
-            parent.appendChild(element)
-
-class StateRendezvous:
-    def __init__(self):
-        self.status = None
-
-    def set_status(self, status):
-        self.status = status
-
-    #
-    # ...
-    #   <status>failed</status>
-    # ...
-    #
-
-    def marshal(self, document, parent):
-        if self.status:
-            element = document.createElement("status")
-            text = document.createTextNode(self.status)
-            element.appendChild(text)
-            parent.appendChild(element)
-
-ACTIVITIES = [
-    "idle",
-    "rendezvous",
-    "negotiate",
-    "test",
-    "collect",
-]
-
-class State:
-
-    #
-    # We put versioninfo in the constructor because
-    # it should always be visible and should never be
-    # cleared when we change activity.
-    #
+class State(object):
 
     def __init__(self):
-        self.versioninfo = ()
-        self.rendezvous = None
-        self.activity = "idle"
-        self.negotiate = None
-        self.test = None
-        self.since = timestamp()
-        self.next_rendezvous = -1
+        self.current = ""
+        self.events = {}
+        self.t = T()
 
-    def set_next_rendezvous(self, t):
-        self.next_rendezvous = t
-        return self
+        self.update("since", int(time.time()))
+        self.update("pid", os.getpid())
 
-    def set_versioninfo(self, ver, uri):
-        self.versioninfo = (ver, uri)
-        return self
+    def marshal(self, indent=None, t=None):
+        state = vars(self)
 
-    #
-    # <state t="1284818634023">
-    #  <since>1292599259</since>
-    #  <next_rendezvous>1292599277</next_rendezvous>
-    #  <active>True</active>
-    #  <activity>rendezvous</activity>
-    #  <activity>negotiate</activity>
-    #  <activity current="true">test</activity>
-    #  <activity>collect</activity>
-    #  <negotiate>
-    #    ...
-    #  </negotiate>
-    #  <test>
-    #    ...
-    #  </test>
-    # </state>
-    #
-
-    def marshal(self):
-        timestamp = get_event_timestamp(STATECHANGE)
-        document = minidom.parseString("<state/>")
-        root = document.documentElement
-        root.setAttribute("t", timestamp)
-        element = document.createElement("since")
-        root.appendChild(element)
-        text = document.createTextNode(str(self.since))
-        element.appendChild(text)
-        if self.next_rendezvous > 0:
-            element = document.createElement("next_rendezvous")
-            root.appendChild(element)
-            text = document.createTextNode(str(self.next_rendezvous))
-            element.appendChild(text)
-        if len(self.versioninfo) == 2:
-            ver, uri = self.versioninfo
-            if versioncmp(ver, version) > 0:
-                element = document.createElement("update")
-                element.setAttribute("uri", uri)
-                text = document.createTextNode(ver)
-                element.appendChild(text)
-                root.appendChild(element)
-        element = document.createElement("active")
-        if self.activity == "idle":
-            text = document.createTextNode("false")
-        else:
-            text = document.createTextNode("true")
-        element.appendChild(text)
-        root.appendChild(element)
-        element = document.createElement("pid")
-        text = document.createTextNode(str(os.getpid()))
-        element.appendChild(text)
-        root.appendChild(element)
-        for activity in ACTIVITIES:
-            element = document.createElement("activity")
-            if activity == self.activity:
-                element.setAttribute("current", "true")
-            text = document.createTextNode(activity)
-            element.appendChild(text)
-            root.appendChild(element)
-        if self.negotiate:
-            element = document.createElement("negotiate")
-            self.negotiate.marshal(document, element)
-            root.appendChild(element)
-        if self.test:
-            element = document.createElement("test")
-            self.test.marshal(document, element)
-            root.appendChild(element)
-        if self.rendezvous:
-            element = document.createElement("rendezvous")
-            self.rendezvous.marshal(document, element)
-            root.appendChild(element)
-        return XML_to_stringio(document)
-
-    #
-    # Return self because we want to combine
-    # many function calls on the same line,
-    # e.g. state.set_inactive("").commit()
-    #
-
-    def set_inactive(self):
-        self.activity = "idle"
-        return self
-
-    def set_activity(self, activity, tasks=[], name=""):
-        if not activity in ACTIVITIES:
-            raise ValueError("Invalid activity name: %s" % activity)
         #
-        # When there is an Idle -> Busy transition we forget
-        # the results of the previous test. We keep them while
-        # we're idle so: (a) if an UI attaches while we are
-        # idle it could show something; (b) there is not a race
-        # condition between the browser and the Collect ->
-        # Idle transition (that used to be the place where we
-        # forgot the results).
+        # If the client passes us via `t` the opaque time of the
+        # latest state change, we can try to reduce the amount of
+        # information we return.
+        # This should reduce the neubot-to-browser traffic when
+        # the neubot agent is idle.
         #
-        if self.activity == "idle":
-            self.rendezvous = None
-            self.negotiate = None
-            self.test = None
-        self.activity = activity
-        if activity == "negotiate":
-            self.negotiate = StateNegotiate()
-        elif activity == "test":
-            self.test = StateTest(name, tasks)
-        elif activity == "collect":
-            self.test.complete()
-        return self
 
-    def set_task(self, task):
-        if self.test:
-            self.test.set_task(task)
-        return self
+        if t:
+            t = int(t)
+            evts = {}
+            state = {}
 
-    def set_queueInfo(self, queuePos, queueLen):
-        if self.negotiate:
-            self.negotiate.set_queueInfo(queuePos, queueLen)
-        return self
+            for name, value in self.events.items():
+                q, event = value
+                if q > t:
+                    evts[name] = event
 
-    def set_rendezvous_status(self, status):
-        if not self.rendezvous:
-            self.rendezvous = StateRendezvous()
-        self.rendezvous.set_status(status)
-        return self
+            if len(evts) > 0:
+                state["current"] = self.current
+                state["events"] = evts
+                state["t"] = self.t
 
-    def append_result(self, tag, value, unit):
-        if self.test:
-            self.test.append_result(tag, value, unit)
-        return self
+        return json.dumps(state, indent=indent)
 
-    def commit(self):
-        publish(STATECHANGE)
-        return self
+    def update(self, name, event=None, publish=True):
+        if event == None:
+            event = {}
 
-#
-# We expect to be used as follows::
-#  from neubot import state
-#  ...
-#  state.set_inactive().commit()
-#
+        self.current = name
+        self.t = T()
+        self.events[name] = self.t, event
 
-state = State()
+        if publish:
+            NOTIFIER.publish(STATECHANGE, self.t)
 
-#
-# Unit testing
-#
 
-from sys import stdout
+STATE = State()
 
 if __name__ == "__main__":
-    PRINT = lambda: stdout.write(state.marshal().read() + "\n")
-    TASKS = ["latency", "download", "upload"]
-    count = 0
-    state.set_versioninfo("0.7.3", "http://packages.neubot.org/latest")
-    PRINT()
-    while count < 2:
-        state.set_activity("negotiate").set_queueInfo("2", "3").commit()
-        PRINT()
-        state.set_queueInfo("1", "3").commit()
-        PRINT()
-        state.set_activity("test", TASKS).set_task("latency").commit()
-        PRINT()
-        state.append_result("latency", 0.013, "s")
-        state.set_task("download").commit()
-        PRINT()
-        state.append_result("download", 11.1, "Mbit/s")
-        state.set_task("upload").commit()
-        PRINT()
-        state.append_result("upload", 9.8, "Mbit/s")
-        state.set_activity("collect").commit()
-        PRINT()
-        state.set_inactive()
-        PRINT()
-        count = count + 1
+    STATE.update("update", {"version": "0.4.1",
+      "uri": "http://www.example.com/"})
+    STATE.update("idle")
+    print STATE.marshal(indent=4)
+
+    STATE.update("rendezvous")
+    print STATE.marshal(indent=4)
+
+    STATE.update("rendezvous", {"status": "failed"})
+    print STATE.marshal(indent=4)
+
+    STATE.update("idle")
+    print STATE.marshal(indent=4)
+
+    STATE.update("rendezvous")
+    print STATE.marshal(indent=4)
+
+    STATE.update("negotiate", {"queue_pos": 3, "queue_len": 7})
+    print STATE.marshal(indent=4)
+
+    STATE.update("negotiate", {"queue_pos": 2, "queue_len": 8})
+    print STATE.marshal(indent=4)
+
+    STATE.update("negotiate", {"queue_pos": 1, "queue_len": 7})
+    print STATE.marshal(indent=4)
+
+    STATE.update("speedtest_latency", {"avg": 0.013, "unit": "s"})
+    print STATE.marshal(indent=4)
+
+    STATE.update("speedtest_download", {"avg": 6.94, "unit": "Mbit/s"})
+    print STATE.marshal(indent=4)
+
+    STATE.update("speedtest_upload", {"avg": 0.97, "unit": "Mbit/s"})
+    print STATE.marshal(indent=4)
+
+    STATE.update("collect")
+    print STATE.marshal(indent=4)
+
+    STATE.update("idle")
+    print STATE.marshal(indent=4)
