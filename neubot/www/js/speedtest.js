5da9b6f18a9d0d8452b55855e497c1f6b52ab691
Simone Basso <bassosimone@gmail.com>
2011-03-05 17:04:19 +0100

    speedtest.js: Note we should use a larger font for axis labels
    Suggested by Antonio.  Don't have time to do that right now, so just
    adding a comment to the code.

--- neubot/www/js/speedtest.js
+++ neubot/www/js/speedtest.js
@@ -205,6 +205,8 @@ var speedtest = (function() {
 
         var min = Math.ceil((since - utils.getNow()) / (60 * 60 * 1000));
 
+        // TODO We should use a larger font for axis labels
+
         mydata = downloadData.concat(uploadData);
         var plot = jQuery.jqplot("chartdiv1", mydata, {
           title: {
