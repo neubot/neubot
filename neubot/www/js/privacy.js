b2e19ad025a0cc64cf0ae5292551622d4aea0d39
Alessio Palmero Aprosio <alessio@apnetwork.it>
2011-04-21 18:09:25 +0200

    www: privacy now works

---
+++ neubot/www/js/privacy.js
@@ -0,0 +1,78 @@
+/* neubot/www/js/privacy.js */
+/*-
+ * Copyright (c) 2011 Alessio Palmero Aprosio <alessio@apnetwork.it>,
+ *  Universita' degli Studi di Milano
+ *
+ * This file is part of Neubot <http://www.neubot.org/>.
+ *
+ * Neubot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Neubot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+function setPrivacy() {
+    setConfigVars({
+        'privacy.informed': jQuery("#check_privacy_informed").attr("checked") ? 1 : 0,
+        'privacy.can_collect': jQuery("#check_privacy_can_collect").attr("checked") ? 1 : 0,
+        'privacy.can_share': jQuery("#check_privacy_can_share").attr("checked") ? 1 : 0
+    });
+    alert("Privacy settings successfully saved");
+    return false;
+}
+
+function checkPrivacy(data) {
+    if (data['privacy.informed']) {
+        jQuery("#check_privacy_informed").attr("checked", "checked");
+    }
+    if (data['privacy.can_collect']) {
+        jQuery("#check_privacy_can_collect").attr("checked", "checked");
+    }
+    if (data['privacy.can_share']) {
+        jQuery("#check_privacy_can_share").attr("checked", "checked");
+    }
+    return false;
+}
+
+jQuery(document).ready(function() {
+    utils.setActiveTab("privacy");
+    getConfigVars(checkPrivacy);
+    
+    jQuery('#testResultsBox').qtip({
+        content: "A new test is running.",
+        position: {
+            target: jQuery('#testTime'),
+            corner: {
+                tooltip: "rightMiddle",
+                target: "leftMiddle"
+            }
+        },
+        show: {
+            when: false,
+            ready: false
+        },
+        hide: false,
+        style: {
+            border: {
+                width: 2,
+                radius: 5
+            },
+            padding: 10,
+            textAlign: 'center',
+            tip: true,
+            name: 'blue'
+        }
+    });
+
+    tracker = state.tracker();
+    tracker.start();
+
+});
