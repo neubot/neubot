dcfcdbb865f02ca410d62b33dbd906f39c9b07d6
Simone Basso <bassosimone@gmail.com>
2011-05-04 16:27:06 +0200

    speedtest: pass ClientSpeedtest the real configuration

--- neubot/speedtest/__init__.py
+++ neubot/speedtest/__init__.py
@@ -461,10 +461,9 @@ from neubot.speedtest.client import ClientSpeedtest
 class SpeedtestClient2(object):
 
     def __init__(self, uri, nclients, flags, debug=False, parent=None):
-        conf = {
-            "speedtest.client.uri": uri,
-            "speedtest.client.nconn": nclients,
-        }
+        conf = CONFIG.select("speedtest.client")
+        conf["speedtest.client.uri"] = uri
+        conf["speedtest.client.nconn"] = nclients
         client = ClientSpeedtest(POLLER)
         client.configure(conf)
         client.connect_uri()
