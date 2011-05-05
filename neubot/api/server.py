6e5fc76f0c7579d281e8a8ec2f12e8ae230b6b4a
Simone Basso <bassosimone@gmail.com>
2011-05-05 11:25:01 +0200

    api/server.py: Make /api more transparent and debuggable
    This patch makes /api more transparent and debuggable.  We allow
    all APIs to receive an optional parameter debug=BOOL.  Whenever
    this parameter is true, we return an indented JSON string with mime-
    type 'text/plain'.  So you can inspect the state directly from the
    browser.
    
    Note that /api/log?debug=1 does not return a JSON but rather
    plain text.  This is because it's easier to read line-based logs
    rather than eye-parsing the formatted JSON.
    
    While there, this patch also harmonizes all the modules that are
    invoked by the API server.  In particular, now, they all return
    objects.  It is API server responsibility to marshal them according
    to the user-specified policy.
    
    I need this patch right now because I have to make Neubot more
    transparent in order to understand why it blocked under Windows
    in rendezvous state.

--- neubot/api/server.py
+++ neubot/api/server.py
@@ -38,7 +38,7 @@ from neubot.net.poller import POLLER
 from neubot.notify import NOTIFIER
 from neubot.notify import STATECHANGE
 from neubot.state import STATE
-from neubot.utils import timestamp
+from neubot import utils
 
 VERSION = "Neubot 0.3.6\n"
 
@@ -78,16 +78,16 @@ class ServerAPI(ServerHTTP):
         path, query = urlparse.urlsplit(request.uri)[2:4]
 
         if path == "/api/config":
-            self.api_config(stream, request)
+            self.api_config(stream, request, query)
 
         elif path == "/api/configlabels":
-            self.api_configlabels(stream, request)
+            self.api_configlabels(stream, request, query)
 
         elif path == "/api/exit":
             self.api_exit(stream, request)
 
         elif path == "/api/log":
-            self.api_log(stream, request)
+            self.api_log(stream, request, query)
 
         elif path == "/api/speedtest":
             self.api_speedtest(stream, request, query)
@@ -107,8 +107,13 @@ class ServerAPI(ServerHTTP):
                     body=StringIO.StringIO("404 Not Found"))
             stream.send_response(request, response)
 
-    def api_config(self, stream, request):
+    def api_config(self, stream, request, query):
         response = Message()
+
+        indent, mimetype, sort_keys = None, "application/json", False
+        dictionary = cgi.parse_qs(query)
+        if "debug" in dictionary and utils.intify(dictionary["debug"][0]):
+            indent, mimetype, sort_keys = 4, "text/plain", True
 
         if request.method == "POST":
             s = request.body.read()
@@ -118,32 +123,52 @@ class ServerAPI(ServerHTTP):
             # Empty JSON b/c '204 No Content' is treated as an error
             s = "{}"
         else:
-            s = json.dumps(CONFIG.conf)
+            s = json.dumps(CONFIG.conf, sort_keys=sort_keys, indent=indent)
 
         stringio = StringIO.StringIO(s)
         response.compose(code="200", reason="Ok", body=stringio,
-                         mimetype="application/json")
+                         mimetype=mimetype)
         stream.send_response(request, response)
 
-    def api_configlabels(self, stream, request):
+    def api_configlabels(self, stream, request, query):
+
+        indent, mimetype = None, "application/json"
+        dictionary = cgi.parse_qs(query)
+        if "debug" in dictionary and utils.intify(dictionary["debug"][0]):
+            indent, mimetype = 4, "text/plain"
+
         response = Message()
-        s = json.dumps(CONFIG.descriptions, sort_keys=True)
+        s = json.dumps(CONFIG.descriptions, sort_keys=True, indent=indent)
         stringio = StringIO.StringIO(s)
         response.compose(code="200", reason="Ok", body=stringio,
-                         mimetype="application/json")
+                         mimetype=mimetype)
         stream.send_response(request, response)
 
-    def api_log(self, stream, request):
+    def api_log(self, stream, request, query):
+
         response = Message()
-        s = json.dumps(LOG.serialize())
-        stringio = StringIO.StringIO(s)
+
+        dictionary = cgi.parse_qs(query)
+        if "debug" in dictionary and utils.intify(dictionary["debug"][0]):
+            stringio = StringIO.StringIO()
+            for ln in LOG.listify():
+                ln = map(str, ln)
+                stringio.write(" ".join(ln))
+                stringio.write("\r\n")
+            stringio.seek(0)
+            mimetype = "text/plain"
+        else:
+            s = json.dumps(LOG.listify())
+            stringio = StringIO.StringIO(s)
+            mimetype = "application/json"
+
         response.compose(code="200", reason="Ok", body=stringio,
-                         mimetype="application/json")
+                         mimetype=mimetype)
         stream.send_response(request, response)
 
     def api_speedtest(self, stream, request, query):
         since = 0
-        until = timestamp()
+        until = utils.timestamp()
 
         dictionary = cgi.parse_qs(query)
         if dictionary.has_key("since"):
@@ -155,10 +180,17 @@ class ServerAPI(ServerHTTP):
             if until < 0:
                 raise ValueError("Invalid query string")
 
+        indent, mimetype, sort_keys = None, "application/json", False
+        dictionary = cgi.parse_qs(query)
+        if "debug" in dictionary and utils.intify(dictionary["debug"][0]):
+            indent, mimetype, sort_keys = 4, "text/plain", True
+
         response = Message()
-        stringio = database.dbm.query_results_json(since, until)
+        lst = database.dbm.query_results_list(since, until)
+        s = json.dumps(lst, indent=indent, sort_keys=sort_keys)
+        stringio = StringIO.StringIO(s)
         response.compose(code="200", reason="Ok", body=stringio,
-                         mimetype="application/json")
+                         mimetype=mimetype)
         stream.send_response(request, response)
 
     def api_state(self, stream, request, query):
@@ -177,11 +209,18 @@ class ServerAPI(ServerHTTP):
 
     def api_state_complete(self, event, context):
         stream, request, query, t = context
-        octets = STATE.marshal(t=t)
+
+        indent, mimetype = None, "application/json"
+        dictionary = cgi.parse_qs(query)
+        if "debug" in dictionary and utils.intify(dictionary["debug"][0]):
+            indent, mimetype = 4, "text/plain"
+
+        dictionary = STATE.dictionarize(t=t)
+        octets = json.dumps(dictionary, indent=indent)
         stringio = StringIO.StringIO(octets)
         response = Message()
         response.compose(code="200", reason="Ok", body=stringio,
-                         mimetype="application/json")
+                         mimetype=mimetype)
         stream.send_response(request, response)
 
     def api_testnow(self, stream, request, query):
