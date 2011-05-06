afe7c7ad711a332c8b5df94590169a845dc038c0
Simone Basso <bassosimone@gmail.com>
2011-05-06 16:12:25 +0200

    Copy rendezvous client code to rendezvous/client.py
    While there, use neubot/config.py and neubot/boot.py to shrink the
    code base and for consistency with other commands.

--- neubot/main.py
+++ neubot/main.py
@@ -229,6 +229,7 @@ MODULES = {
     "http"       : "http.client",
     "httpd"      : "http.server",
     "rendezvous" : "rendezvous",
+    "rendezvous.client": "rendezvous.client",
     "rendezvous.server": "rendezvous.server",
     "speedtest"  : "speedtest",
     "statusicon" : "statusicon",
