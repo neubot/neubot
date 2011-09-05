4f86b4a3a7fe254de90c08fd540f61f467437dc9
Simone Basso <bassosimone@gmail.com>
2011-09-05 14:15:23 +0200

    database/table_config.py: add docstrings

--- neubot/database/table_config.py
+++ neubot/database/table_config.py
@@ -20,38 +20,39 @@
 # along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+'''
+ Config table.
+ The config table is like a dictionary and keeps some useful
+ variables.  It keeps the version of the database format, that
+ might be useful in the future to convert an old database to
+ a new one, should we change the format.
+
+ <on uuid and privacy>
+ It also keeps an unique identifier for each neubot client
+ which we believe would help data analysis, e.g. we could
+ review the measurement history of a given neubot looking for
+ certain patterns, such as the connection speed decreasing
+ near the end of the month because the user exceeded a
+ bandwidth cap.
+
+ We believe this should have a negligible impact on privacy
+ because it would, at most, allow to say that neubot XYZ owner
+ changed IP address, and hence, possibly, provider and/or
+ location (and note that this information is functional to
+ our goal of building a network neutrality map organized by
+ geographic location and provider).
+
+ So, we believe you should not be concerned by this issue,
+ but, in case you were, the way to go is `neubot database -f`
+ that forces neubot to generate and use A NEW uuid.
+ </on uuid and privacy>
+'''
+
 from neubot.utils import get_uuid
 from neubot import compat
 
-#
-# Config table.
-# The config table is like a dictionary and keeps some useful
-# variables.  It keeps the version of the database format, that
-# might be useful in the future to convert an old database to
-# a new one, should we change the format.
-#
-# <on uuid and privacy>
-# It also keeps an unique identifier for each neubot client
-# which we believe would help data analysis, e.g. we could
-# review the measurement history of a given neubot looking for
-# certain patterns, such as the connection speed decreasing
-# near the end of the month because the user exceeded a
-# bandwidth cap.
-#
-# We believe this should have a negligible impact on privacy
-# because it would, at most, allow to say that neubot XYZ owner
-# changed IP address, and hence, possibly, provider and/or
-# location (and note that this information is functional to
-# our goal of building a network neutrality map organized by
-# geographic location and provider).
-#
-# So, we believe you should not be concerned by this issue,
-# but, in case you were, the way to go is `neubot database -f`
-# that forces neubot to generate and use A NEW uuid.
-# </on uuid and privacy>
-#
-
 def create(connection, commit=True):
+    ''' Creates table_config if it does not exist '''
     connection.execute("""CREATE TABLE IF NOT EXISTS config(
       name TEXT PRIMARY KEY, value TEXT);""")
     connection.execute("""INSERT OR IGNORE INTO config VALUES(
@@ -62,6 +63,7 @@ def create(connection, commit=True):
         connection.commit()
 
 def update(connection, iterator, commit=True, clear=False):
+    ''' Updates table config with iterator '''
     if clear:
         connection.execute("DELETE FROM config;")
     connection.executemany("""INSERT OR REPLACE INTO config VALUES(
@@ -70,6 +72,7 @@ def update(connection, iterator, commit=True, clear=False):
         connection.commit()
 
 def walk(connection, func):
+    ''' Walks over table_config in functional style '''
     cursor = connection.cursor()
     cursor.execute("SELECT name, value FROM CONFIG;")
     #
@@ -81,9 +84,11 @@ def walk(connection, func):
       row["value"]])), cursor)
 
 def dictionarize(connection):
-    d = {}
-    walk(connection, lambda t: d.update([t]))
-    return d
+    ''' Dictionarize table_config '''
+    dictionary = {}
+    walk(connection, lambda t: dictionary.update([t]))
+    return dictionary
 
 def jsonize(connection):
+    ''' Jsonize table_config '''
     return compat.json.dumps(dictionarize(connection))
