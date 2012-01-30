627e07dd83cbb780abac22ecf0ee2c8366ac4ead
Simone Basso <bassosimone@gmail.com>
2012-01-30 12:36:09 +0100

    state.js: document the code

--- neubot/www/js/state.js
+++ neubot/www/js/state.js
@@ -21,6 +21,14 @@
  * along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+//
+// This file contains the code that tracks the state of the Neubot
+// daemon and updates current test information in the right sidebar
+// of the web user interface.
+// Further processing is possible by passing the tracker() method
+// a callback function.
+//
+
 var state = (function() {
     var state_ctx = {};
 
@@ -34,29 +42,39 @@ var state = (function() {
         //
         // In case of error keep trying because it might just
         // be that the user is restarting neubot, but also add
-        // a significant delay to let the browser breathe
+        // a significant delay to let the browser breathe.
         //
         function get_state_error() {
             setTimeout(get_state, 5000);
         }
 
+        // Update the right sidebar using current state
         function update_sidebar(data) {
+
+            // Update the state of automatic tests
             if (data.events.config) {
                 if (data.events.config.enabled != undefined) {
                     utils.setStatusLabels(data.events.config);
                 }
             }
 
+            // Honor information on available updates
             if (data.events.update && data.events.update.uri
               && data.events.update.version) {
                 jQuery("#updateUrl").attr("href", data.events.update.uri);
                 jQuery("#updateUrl").text(data.events.update.uri);
                 jQuery("#updateVersion").text(data.events.update.version);
+
+                // Show update information nicely
                 setTimeout(function() {
                     jQuery('#update').slideDown("slow");
                 }, 500);
             }
 
+            //
+            // This updates latest/current test results in the
+            // right sidebar.
+            //
             if (data.events.test_name) {
                 jQuery("#testNameSideBar").text(data.events.test_name);
             }
@@ -70,7 +88,10 @@ var state = (function() {
                 jQuery("#uploadResult").text(data.events.test_upload);
             }
 
+            // Filter out interesting actions only
             if (in_array(data.current, state.actions)) {
+
+                // Do we need to show or hide the qtip?
                 if (data.current == "test") {
                     jQuery('#testResultsBox').qtip("show");
                     jQuery('#testResultsBox h4').text(i18n.get(
@@ -81,6 +102,11 @@ var state = (function() {
                     jQuery('#testResultsBox h4').text(i18n.get(
                       "Latest test results"));
                 }
+
+                //
+                // Highlight the current state in the
+                // index.html web page.
+                //
                 jQuery('table#state tr').css('background-color',
                   'transparent');
                 jQuery('table#state tr#' + data.current).css(
@@ -88,11 +114,19 @@ var state = (function() {
             }
         }
 
+        //
+        // Invoked when GET /api/state?t=T succeeds.
+        // This function performs some more checks than needed
+        // on the incoming data, for extra robustness.
+        //
         function get_state_success(data) {
+
+            // Always provide a nonempty events dictionary
             if (!data.events) {
                 data.events = {};
             }
 
+            // Always provide next rendezvous information
             if (data.events.next_rendezvous) {
                 next_rendezvous = data.events.next_rendezvous;
             }
@@ -100,6 +134,7 @@ var state = (function() {
                 data.events.next_rendezvous = next_rendezvous;
             }
 
+            // Update time of latest event
             if (data.t) {
                 var t = data.t;
                 if (curtime == undefined) {
@@ -108,13 +143,29 @@ var state = (function() {
                 }
                 curtime = t;
             }
+
+            //
+            // Update the sidebar and, if a callback was provided,
+            // pass it the state for further processing.
+            //
             update_sidebar(data);
             if (callback != undefined) {
                 callback(data);
             }
+
+            //
+            // Use comet and send out immediately another request
+            //
             setTimeout(get_state, 0);
         }
 
+        //
+        // Request the current state of the Neubot daemon and pass
+        // along the time of the latest change.
+        // If there are no changes since the latest change the daemon
+        // will delay the response until either something changes or
+        // a timeout expires.
+        //
         function get_state() {
             var params = {
                 url: "/api/state?t=" + curtime,
@@ -125,11 +176,12 @@ var state = (function() {
             jQuery.ajax(params);
         }
 
-        //
-        // Google Chrome tab icon will keep spinning unless we
-        // delay the first get_state a bit
-        //
         tracker_ctx.start = function() {
+
+            //
+            // Create a qtip that will be shown when a test
+            // is in progress.
+            //
             jQuery('#testResultsBox').qtip({
                 content: i18n.get("Test running"),
                 position: {
@@ -156,7 +208,16 @@ var state = (function() {
                 }
             });
 
+            //
+            // Fetch information on whether automatic tests are
+            // enabled and update the user interface accordingly.
+            //
             utils.getConfigVars(utils.setStatusLabels);
+
+            //
+            // Google Chrome tab icon will keep spinning unless we
+            // delay the first get_state() a bit.
+            //
             setTimeout(get_state, 100);
         }
 
