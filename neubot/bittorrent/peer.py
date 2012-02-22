b9980b2b5b15cf334bcef0fd2843b36231f2f9db
Simone Basso <bassosimone@gmail.com>
2012-02-22 11:36:07 +0100

    bittorrent/peer.py: comment nit

--- neubot/bittorrent/peer.py
+++ neubot/bittorrent/peer.py
@@ -56,7 +56,7 @@ STATES = (INITIAL, SENT_INTERESTED, DOWNLOADING, UPLOADING,
 #
 # This class implements the test finite state
 # machine and message exchange that are documented
-# in <doc/bittorrent/peer.png>.
+# by <doc/bittorrent/peer.png>.
 #
 class PeerNeubot(StreamHandler):
     def __init__(self, poller):
