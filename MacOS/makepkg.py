b810740bebd54a3b20a9d0bb6061bb30cf837862
Simone Basso <bassosimone@gmail.com>
2012-05-11 20:49:44 +0200

    Neubot/0.4.12-rc1 released

--- MacOS/makepkg.py
+++ MacOS/makepkg.py
@@ -43,8 +43,8 @@ TOPDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MACOSDIR = os.sep.join([TOPDIR, 'MacOS'])
 os.chdir(MACOSDIR)
 
-VERSION = '0.4.11'
-NUMERIC_VERSION = '0.004011999'
+VERSION = '0.4.12-rc1'
+NUMERIC_VERSION = '0.004012001'
 
 IGNORER = shutil.ignore_patterns('.DS_Store')
 
