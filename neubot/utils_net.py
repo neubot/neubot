28eb3db7e0aceb9c9096aabfcf9384e7cb6e900c
Simone Basso <bassosimone@gmail.com>
2012-05-13 13:47:09 +0200

    utils_net.py: listen(): cope with epnt[0] being None

--- neubot/utils_net.py
+++ neubot/utils_net.py
@@ -97,7 +97,7 @@ def listen(epnt):
         epnt = (None, epnt[1])
 
     # Allow to listen on a list of addresses
-    if ' ' in epnt[0]:
+    if epnt[0] and ' ' in epnt[0]:
         for address in epnt[0].split():
             result = listen((address.strip(), epnt[1]))
             sockets.extend(result)
