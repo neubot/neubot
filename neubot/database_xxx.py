7329c113545386ac58f9573b844f76b6afd4a3b9
Simone Basso <bassosimone@gmail.com>
2012-05-27 20:48:38 +0200

    add database_xxx.py: database quirks

---
+++ neubot/database_xxx.py
@@ -0,0 +1,52 @@
+# neubot/database_xxx.py
+
+#
+# Copyright (c) 2012 Simone Basso <bassosimone@gmail.com>,
+#  NEXA Center for Internet & Society at Politecnico di Torino
+#
+# This file is part of Neubot <http://www.neubot.org/>.
+#
+# Neubot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Neubot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+''' Database quirks '''
+
+import logging
+import os
+import sys
+
+def linux_fixup_databasedir():
+    ''' Under Linux move database from /var/neubot to /var/lib/neubot '''
+    # Explanation: /var/lib/neubot is FHS, /var/neubot isn't
+
+    if os.name != 'posix':
+        return
+    if not sys.platform.startswith('linux'):
+        return
+    if os.getuid() != 0:
+        return
+
+    if not os.path.isfile('/var/neubot/database.sqlite3'):
+        return
+
+    logging.debug('database_xxx: /var/neubot -> /var/lib/neubot...')
+
+    # Delayed
+    from neubot import utils_posix
+
+    utils_posix.mkdir_idempotent('/var/lib/neubot')
+    os.link('/var/neubot/database.sqlite3', '/var/lib/neubot/database.sqlite3')
+    os.unlink('/var/neubot/database.sqlite3')
+
+    logging.debug('database_xxx: /var/neubot -> /var/lib/neubot... done')
