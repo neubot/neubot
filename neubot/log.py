6ce70d4899cf167515a569a59fe1217ecda1c80a
Simone Basso <bassosimone@gmail.com>
2012-06-10 12:02:33 +0200

    log.py: streaming feature: explain why we UTF8-encode log lines

--- neubot/log.py
+++ neubot/log.py
@@ -143,6 +143,7 @@ class StreamingLogger(object):
             try:
 
                 logline = "%s %s\r\n" % (severity, message)
+                # UTF-8 encoding avoids an oops() in stream.py
                 logline = logline.encode("utf-8")
                 for stream in self.streams:
                     stream.start_send(logline)
