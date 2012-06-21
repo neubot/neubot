dcd5dd5f18641b814f76f2e628b11396c4bf92a9
Simone Basso <bassosimone@gmail.com>
2012-06-21 21:40:12 +0200

    utils_sysdirs.py: bump copyright, add history

--- neubot/utils_sysdirs.py
+++ neubot/utils_sysdirs.py
@@ -1,7 +1,7 @@
 # neubot/utils_sysdirs.py
 
 #
-# Copyright (c) 2011 Simone Basso <bassosimone@gmail.com>,
+# Copyright (c) 2011-2012 Simone Basso <bassosimone@gmail.com>,
 #  NEXA Center for Internet & Society at Politecnico di Torino
 #
 # This file is part of Neubot <http://www.neubot.org/>.
@@ -21,6 +21,8 @@
 #
 
 ''' Location of system-dependent directories '''
+
+# Formerly neubot/rootdir.py
 
 import os
 import sys
