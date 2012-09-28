6723ff786d553e1c9c93bc02e1aceb83da131a5e
Simone Basso <bassosimone@gmail.com>
2012-09-28 15:39:45 +0200

    notify: fix pylint warnings
    While there, use logging exc_info feature in place of asyncore.

--- neubot/notify.py
+++ neubot/notify.py
@@ -1,8 +1,9 @@
 # neubot/notify.py
 
 #
-# Copyright (c) 2010-2011 Simone Basso <bassosimone@gmail.com>,
-#  NEXA Center for Internet & Society at Politecnico di Torino
+# Copyright (c) 2010-2012
+#     Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
+#     and Simone Basso <bassosimone@gmail.com>
 #
 # This file is part of Neubot <http://www.neubot.org/>.
 #
@@ -20,42 +21,45 @@
 # along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+''' Subscribe/publish events '''
+
 #
-# Subscribe/publish events
 # Initially an event has a timestamp of zero, and each time
 # you publish an event its timestamp is updated to the current
 # time, using T().
 #
 
-import asyncore
 import collections
 import logging
 
-from neubot.net.poller import POLLER
-from neubot.utils import T
+from neubot.poller import POLLER
+from neubot import utils
 
 INTERVAL = 60
 
 class Notifier(object):
+    ''' Notify events '''
+
     def __init__(self):
         self._timestamps = collections.defaultdict(int)
         self._subscribers = collections.defaultdict(list)
         self._tofire = []
-
         POLLER.sched(INTERVAL, self._periodic)
 
     def subscribe(self, event, func, context=None, periodic=False):
+        ''' Subscribe to event '''
         queue = self._subscribers[event]
         queue.append((func, context))
         if periodic:
             self._tofire.append(event)
 
-    def publish(self, event, t=None):
-        if not t:
-            t = T()
-        self._timestamps[event] = t
+    def publish(self, event, tsnap=None):
+        ''' Publish event '''
+        if not tsnap:
+            tsnap = utils.T()
+        self._timestamps[event] = tsnap
 
-        logging.debug("* publish: %s", event)
+        logging.debug("notify: publish event: %s", event)
 
         #
         # WARNING! Please resist the temptation of merging
@@ -68,10 +72,11 @@ class Notifier(object):
 
         self._fireq(event, queue)
 
-    def _periodic(self, *args, **kwargs):
+    def _periodic(self):
+        ''' Periodically generate notification for old events '''
         POLLER.sched(INTERVAL, self._periodic)
-        self._tofire, q = [], self._tofire
-        for event in q:
+        self._tofire, queue = [], self._tofire
+        for event in queue:
 
             #
             # WARNING! Please resist the temptation of merging
@@ -82,33 +87,39 @@ class Notifier(object):
             queue = self._subscribers[event]
             del self._subscribers[event]
 
-            logging.debug("* periodic: %s", event)
+            logging.debug("notify: periodically publish event: %s", event)
 
             self._fireq(event, queue)
 
-    def _fireq(self, event, queue):
+    @staticmethod
+    def _fireq(event, queue):
+        ''' Notify all funcs in queue of event '''
         for func, context in queue:
             try:
                 func(event, context)
             except (KeyboardInterrupt, SystemExit):
                 raise
             except:
-                logging.error(str(asyncore.compact_traceback()))
+                logging.error('notify: event func() failed', exc_info=1)
 
     def is_subscribed(self, event):
+        ''' Returns True if the event is subscribed '''
         return event in self._subscribers
 
     def get_event_timestamp(self, event):
+        ''' Returns the timestamp bound to an event '''
         return str(self._timestamps[event])
 
     def needs_publish(self, event, timestamp):
+        ''' Returns True if an event needs to be published '''
         timestamp = int(timestamp)
         if timestamp < 0:
             raise ValueError("Invalid timestamp")
         return timestamp == 0 or timestamp < self._timestamps[event]
 
-    def snap(self, d):
-        d['notifier'] = {'_timestamps': dict(self._timestamps),
+    def snap(self, dct):
+        ''' Take a snapshot of this object '''
+        dct['notifier'] = {'_timestamps': dict(self._timestamps),
           '_subscribers': dict(self._subscribers)}
 
 NOTIFIER = Notifier()
