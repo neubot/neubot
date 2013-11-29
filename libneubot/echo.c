87b370097bdc3a5b101197b08aab05569e24bb28
Simone Basso <bassosimone@gmail.com>
2013-11-29 16:11:09 +0100

    Add echo.c echo server and teach Makefile how to compile it

---
+++ libneubot/echo.c
@@ -0,0 +1,189 @@
+/* libneubot/echo.c */
+
+/*-
+ * Copyright (c) 2013
+ *     Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
+ *     and Simone Basso <bassosimone@gmail.com>
+ *
+ * This file is part of Neubot <http://www.neubot.org/>.
+ *
+ * Neubot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Neubot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Neubot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <event2/buffer.h>
+#include <event2/util.h>
+
+#include "log.h"
+#include "poller.h"
+#include "utils.h"
+
+struct Connection {
+	struct NeubotPollable *pollable;
+	struct evbuffer *buffer;
+	int fileno;
+};
+
+struct NeubotEchoServer {
+	struct NeubotPollable *pollable;
+	struct NeubotPoller *poller;
+	int fileno;
+};
+
+#define MAXREAD 8000
+
+/*
+ * Connection implementation
+ */
+
+static void
+connection_read(void *opaque)
+{
+	struct Connection *self;
+	int result;
+
+	self = (struct Connection *) opaque;
+	result = evbuffer_read(self->buffer, self->fileno, MAXREAD);
+	if (result <= 0) {
+		neubot_pollable_close(self->pollable);
+		return;
+	}
+
+	neubot_pollable_set_writable(self->pollable);
+}
+
+static void
+connection_write(void *opaque)
+{
+	struct Connection *self;
+	int result;
+
+	self = (struct Connection *) opaque;
+	result = evbuffer_write(self->buffer, self->fileno);
+	if (result == -1) {
+		neubot_pollable_close(self->pollable);
+		return;
+	}
+
+	if (evbuffer_get_length(self->buffer) == 0)
+		neubot_pollable_unset_writable(self->pollable);
+}
+
+static void
+connection_close(void *opaque)
+{
+	struct Connection *self;
+
+	/*
+	 * Note: in this function we don't neubot_pollable_close() because
+	 * this function is invoked by neubot_pollable_close().
+	 */
+
+	self = (struct Connection *) opaque;
+	if (self == NULL)
+		return;
+
+	if (self->fileno != -1)
+		(void) evutil_closesocket(self->fileno);
+	if (self->buffer != NULL)
+		evbuffer_free(self->buffer);
+
+	free(self);
+}
+
+static void
+connection_construct(void *opaque)
+{
+	struct Connection *conn;
+	struct NeubotEchoServer *self;
+	int result;
+
+	self = (struct NeubotEchoServer *) opaque;
+
+	conn = calloc(1, sizeof (*conn));
+	if (conn == NULL)
+		goto cleanup;
+
+	conn->fileno = accept(self->fileno, NULL, NULL);
+	if (conn->fileno == -1)
+		goto cleanup;
+
+	conn->buffer = evbuffer_new();
+	if (conn->buffer == NULL)
+		goto cleanup;
+
+	conn->pollable = neubot_pollable_construct(self->poller,
+	    connection_read, connection_write, connection_close, conn);
+	if (conn->pollable == NULL)
+		goto cleanup;
+
+	neubot_pollable_attach(conn->pollable, (long long) conn->fileno);
+
+	result = neubot_pollable_set_readable(conn->pollable);
+	if (result != -1)
+		return;		/* success */
+
+cleanup:
+	connection_close(conn);		/* FIXME: leak */
+}
+
+/*
+ * NeubotEchoServer implementation
+ */
+
+struct NeubotEchoServer *
+neubot_echo_server_construct(struct NeubotPoller *poller, int use_ipv6,
+    const char *address, const char *port)
+{
+	struct NeubotEchoServer *self;
+	int result;
+
+	self = calloc(1, sizeof (*self));
+	if (self == NULL)
+		goto cleanup;
+
+	self->fileno = neubot_listen(use_ipv6, address, port);
+	if (self->fileno == -1)
+		goto cleanup;
+
+	self->poller = poller;
+
+	self->pollable = neubot_pollable_construct(self->poller,
+	    connection_construct, NULL, NULL, self);
+	if (self->pollable == NULL)
+		goto cleanup;
+
+	neubot_pollable_attach(self->pollable, (long long) self->fileno);
+
+	result = neubot_pollable_set_readable(self->pollable);
+	if (result == -1)
+		goto cleanup;
+
+	return (self);
+
+cleanup:
+	if (self != NULL && self->pollable != NULL)
+		neubot_pollable_close(self->pollable);
+	if (self != NULL && self->fileno != -1)
+		(void) evutil_closesocket(self->fileno);
+	if (self != NULL && self != NULL)
+		free(self);
+	return (NULL);
+}
